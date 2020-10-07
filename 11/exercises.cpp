d7dc11ead23648ac925ceb59842727496830cdde
William E. Skeith <wes@cs.ccny.cuny.edu>
2020-10-07 17:23:28 -0400

    More exercises.

---
+++ 11/exercises.cpp
@@ -0,0 +1,5 @@
+/* TODO: try to write the polynomial evaluation function we started
+ * in lecture. */
+/* TODO: actually write down (and test) push_front and pop_front
+ * (we outlined them in class, but it may still be instructive to
+ * actually write it down yourself). */
